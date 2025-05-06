8d36ab606affec224b11f4eead415a2ed3a448c6
maryori190909 <maryori.armengod@estudiante.ucsm.edu.pe>
2025-05-06 16:02:45 -0500

    v5

--- Actividad1/StackArray.java
+++ Actividad1/StackArray.java
@@ -1,5 +1,10 @@
 public class StackArray <E> implements Stack <E>{
     private E[] array;
     private int tope;
+    
+    public StackArray (int capacidad){
+        this.array =(E[]) new Object[capacidad];
+        this.tope = -1;
+    }
 
 }
