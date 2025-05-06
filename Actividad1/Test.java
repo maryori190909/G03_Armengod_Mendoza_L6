a0649e0b469cbc6f0e0cf43818e08de51901bc1a
maryori190909 <maryori.armengod@estudiante.ucsm.edu.pe>
2025-05-06 16:49:37 -0500

    v20

--- Actividad1/Test.java
+++ Actividad1/Test.java
@@ -1,7 +1,14 @@
 
 public class Test {
     public static void main(String[] args) {
-        
+        try{
+            Stack<Integer> pi1= new StackArray<>(4);
+            pi1.push(5);
+            pi1.push(10);
+            pi1.push(15);
+            pi1.push(20);
+            System.out.println(pi1);
+        }
     }
 
 }
