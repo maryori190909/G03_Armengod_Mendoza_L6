45afb3f6769048eb0a0c8e95f001c5d0dfb5caf3
maryori190909 <maryori.armengod@estudiante.ucsm.edu.pe>
2025-05-10 23:51:08 -0500

    v2

--- Ejercicio 4/Aplicacion.java
+++ Ejercicio 4/Aplicacion.java
@@ -1,5 +1,15 @@
 package Ejercicio 4;
 
 public class Aplicacion {
+    public static boolean symbolBalancing(String s) {
+        StackLink<Character> stack = new StackLink<>();
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (ch == '(' || ch == '[' || ch == '{') {
+                stack.push(ch);
+            }
+        }
+
+    }
 
 }
