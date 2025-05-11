42ee81a62191519ab0381faa1ab713ac0126b740
maryori190909 <maryori.armengod@estudiante.ucsm.edu.pe>
2025-05-10 23:03:35 -0500

    v4

--- Ejercicio3/PriorityQueueLinked.java
+++ Ejercicio3/PriorityQueueLinked.java
@@ -1,5 +1,17 @@
 package Ejercicio3;
 
-public class PriorityQueueLinked {
+import Actividad2.QueueLink;
+
+public class PriorityQueueLinked <E> implements PriorityQueue<E, Integer> {
+    private Queue<E>[] queues;
+    private int numP;
+    public PriorityQueueLinked(int numP) {
+        this.numP = numP;
+        this.queues = new Queue[numP];
+        for (int i = 0; i < numP; i++) {
+            queues[i] = new QueueLink<>();
+        }
+    }
+
 
 }
