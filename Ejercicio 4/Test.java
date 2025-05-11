146e707a620e70367cd5b1c9575933d41dab7fe0
maryori190909 <maryori.armengod@estudiante.ucsm.edu.pe>
2025-05-11 00:00:44 -0500

    v final

--- Ejercicio 4/Test.java
+++ Ejercicio 4/Test.java
@@ -4,12 +4,10 @@ public class Test {
 
     public static void main(String[] args) {
         String[] entradas = {
-            "()()()[()]", 
-            "((()))[]", 
-            "[[[]](", 
-            "([{])}", 
-            "(", 
-            "[][[]]{{{}}}"
-        };
+            "()()()[()]", "((()))[]", "[[[]](", "([{])}", "(", "[][[]]{{{}}}"};
+        for (String s : entradas) {
+            boolean resultado = Aplicacion.symbolBalancing(s);
+            System.out.println("Entrada: " + s + " -> " + resultado);
+        }
     }
 }
