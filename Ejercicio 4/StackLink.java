5a6a7b7e94c7d87d386738ca24710dcaf88dbab2
maryori190909 <maryori.armengod@estudiante.ucsm.edu.pe>
2025-05-10 23:55:29 -0500

    v5

--- Ejercicio 4/StackLink.java
+++ Ejercicio 4/StackLink.java
@@ -2,4 +2,12 @@ package Ejercicio 4;
 
 public class StackLink<T> {
 
+    private static class Node<T> {
+        T data;
+        Node<T> next;
+        Node(T data) {
+            this.data = data;
+        }
+    }
+
 }
