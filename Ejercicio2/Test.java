653441b4b03f03f9092999dbdc62faedefac92f8
maryori190909 <maryori.armengod@estudiante.ucsm.edu.pe>
2025-05-10 22:44:00 -0500

    v12

--- Ejercicio2/Test.java
+++ Ejercicio2/Test.java
@@ -3,10 +3,21 @@ package Ejercicio2;
 public class Test {
     public static void main(String[] args) {
         try{
-            Queue<Integer> colaEnteros = new QueueArray<>(3);
-            colaEnteros.enqueue(12);
-            colaEnteros.enqueue(29);
-            colaEnteros.enqueue(5);
+            Queue<Integer> ce = new QueueArray<>(3);
+            ce.enqueue(12);
+            ce.enqueue(29);
+            ce.enqueue(5);
+
+            System.out.println(ce);
+
+            System.out.println("frente: " + ce.front());
+            System.out.println("final: " + ce.back());
+
+            ce.dequeue();
+            System.out.println("cola despues de eliminar un elemento: " + ce);
+
+            ce.enqueue(11);
+            System.out.println("cola depues de agregar 11: " + ce);
         }
     }
 
