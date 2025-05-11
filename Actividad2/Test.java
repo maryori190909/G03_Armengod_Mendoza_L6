f333631897c08df72612a360cefc4f69d025b365
maryori190909 <maryori.armengod@estudiante.ucsm.edu.pe>
2025-05-10 20:30:29 -0500

    v14

--- Actividad2/Test.java
+++ Actividad2/Test.java
@@ -1,9 +1,20 @@
 package Actividad2;
+import java.util.Queue;
+
 import Actividad1.ExceptionIsEmpty;
 
 public class Test {
     public static void main(String[] args) {
-        
+        try{
+            Queue<Integer> cl1 = new QueueLink<>();
+            cl1.enqueue(6);
+            cl1.enqueue(12);
+            cl1.enqueue(18);
+
+            System.out.println(" cola1: "+ cl1);
+            System.out.println(" Frente: "+ cl1.front());
+            System.out.println( "Final: "+ cl1.back());
+        }
     }
 
 }
