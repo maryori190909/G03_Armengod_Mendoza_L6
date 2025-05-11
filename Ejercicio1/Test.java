dc571007b56c27d2388f34c70254d5206c4b7821
maryori190909 <maryori.armengod@estudiante.ucsm.edu.pe>
2025-05-10 22:12:25 -0500

    v11

--- Ejercicio1/Test.java
+++ Ejercicio1/Test.java
@@ -1,5 +1,13 @@
 package Ejercicio1;
 
 public class Test {
+    public static void main(String[] args) {
+        try{
+            Stack<Integer> p1 = new StackLink<>();
+            p1.push(5);
+            p1.push(22);
+            p1.push(19);
+        }
+    }
 
 }
