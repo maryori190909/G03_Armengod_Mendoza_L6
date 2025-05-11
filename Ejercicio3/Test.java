44d3c06bec1a98eabc206cb8f7a9cc2d678e3da8
maryori190909 <maryori.armengod@estudiante.ucsm.edu.pe>
2025-05-10 23:13:29 -0500

    v10

--- Ejercicio3/Test.java
+++ Ejercicio3/Test.java
@@ -1,5 +1,18 @@
 package Ejercicio3;
 
 public class Test {
+    public static void main(String[] args) {
+        PriorityQueue<String, Integer> pq = new PriorityQueueLinked<>(3);
+
+        try {
+            pq.enqueue("tarea A", 0);
+            pq.enqueue("tarea B", 1);
+            pq.enqueue("tarea C", 1);
+            pq.enqueue("tarea D", 2);
+            pq.enqueue("tarea E", 2);
+            pq.enqueue("tarea F", 0);
+
+        }
+    }
 
 }
