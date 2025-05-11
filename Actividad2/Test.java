74512322945d0b6e02dded8c4edfffe109ea8517
maryori190909 <maryori.armengod@estudiante.ucsm.edu.pe>
2025-05-10 20:35:50 -0500

    v15

--- Actividad2/Test.java
+++ Actividad2/Test.java
@@ -14,7 +14,18 @@ public class Test {
             System.out.println(" cola1: "+ cl1);
             System.out.println(" Frente: "+ cl1.front());
             System.out.println( "Final: "+ cl1.back());
+
+            System.out.println(" elminar el elemento del frente:" + cl1.dequeue());
+            System.out.println("Nueva cola: "+ cl1);
+
+            Queue<String> cl2 = new QueueLink<>();
+            cl2.enqueue("Gato");
+            cl2.enqueue(" raton");
+            cl2.enqueue("perro");
+
+            System.out.println("cola2:"+ cl2);
         }
+        
     }
 
 }
