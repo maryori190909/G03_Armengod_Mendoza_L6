1e68622e70b6179d0da0eea2188d8d9a7071d9ad
maryori190909 <maryori.armengod@estudiante.ucsm.edu.pe>
2025-05-10 21:13:41 -0500

    v8

--- Actividad3/PriorityQueueLinkSort.java
+++ Actividad3/PriorityQueueLinkSort.java
@@ -1,5 +1,18 @@
 package Actividad3;
 
-public class PriorityQueueLinkSort {
+public class PriorityQueueLinkSort<E, N extends Comparable<N>> implements PriorityQueue<E, N>{
+    class EntryNode {
+        E data;
+        N priority;
+
+        EntryNode(E data, N priority) {
+            this.data = data;
+            this.priority = priority;
+        }
+
+        public String toString() {
+            return "(" + data + ", la prioridad: " + priority + ")";
+        }
+    }
 
 }
