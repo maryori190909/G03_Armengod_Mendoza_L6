07896ebf775e6601064053d5b846684872a6f0e3
maryori190909 <maryori.armengod@estudiante.ucsm.edu.pe>
2025-05-10 21:07:13 -0500

    v4

--- Actividad3/Node.java
+++ Actividad3/Node.java
@@ -1,5 +1,12 @@
 package Actividad3;
 
-public class Node {
+public class Node<T> {
+    private T data;
+    private Node<T> next;
+
+    public Node(T data){
+        this.data = data;
+        this.next = null;
+    }
 
 }
