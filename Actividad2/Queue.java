021664731e93283ba328a424ac0d0bcd7b4f2800
maryori190909 <maryori.armengod@estudiante.ucsm.edu.pe>
2025-05-10 19:54:25 -0500

    v2

--- Actividad2/Queue.java
+++ Actividad2/Queue.java
@@ -1,3 +1,7 @@
-public interface Queue {
-
+public interface Queue<E> {
+    void enqueue(E x);
+    E dequeue() throws ExceptionIsEmpty;
+    E front() throws ExceptionIsEmpty;
+    E back() throws ExceptionIsEmpty;
+    boolean isEmpty();
 }
