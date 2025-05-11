4da951d67c5a183d9ba72e40b43ccf7c0fef28fa
maryori190909 <maryori.armengod@estudiante.ucsm.edu.pe>
2025-05-10 22:00:16 -0500

    v4

---
+++ Ejercicio1/Node.java
@@ -0,0 +1,12 @@
+package Ejercicio1;
+
+public class Node<E> {
+    private E data;
+    private Node<E> next;
+
+    public Node(E data) {
+        this.data = data;
+        this.next = null;
+    }
+
+}
