53d30bd1b091945f5bca4e39b858ef69a18b9e9e
maryori190909 <maryori.armengod@estudiante.ucsm.edu.pe>
2025-05-10 22:41:04 -0500

    v11

--- Ejercicio2/Test.java
+++ Ejercicio2/Test.java
@@ -1,5 +1,13 @@
 package Ejercicio2;
 
 public class Test {
+    public static void main(String[] args) {
+        try{
+            Queue<Integer> colaEnteros = new QueueArray<>(3);
+            colaEnteros.enqueue(12);
+            colaEnteros.enqueue(29);
+            colaEnteros.enqueue(5);
+        }
+    }
 
 }
