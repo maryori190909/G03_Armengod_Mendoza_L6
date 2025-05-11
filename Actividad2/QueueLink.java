a50914a4cc1755bf8f304dd041d344ebce7e1268
maryori190909 <maryori.armengod@estudiante.ucsm.edu.pe>
2025-05-10 20:10:28 -0500

    v9

--- Actividad2/QueueLink.java
+++ Actividad2/QueueLink.java
@@ -6,7 +6,21 @@ import Actividad1.ExceptionIsEmpty;
 public class QueueLink <E> implements Queue<E>{
     private Node<E> first;
     private Node<E> last;
-    
+
+    public QueueLink(){
+        this.first = null;
+        this.last = null;
+    }
+
+    public void enqueue(E x){
+        Node<E> nuevo =  new Node<>(x);
+        if(isEmpty()){
+            first = nuevo;
+        }else{
+            last.setNext(nuevo);
+        }
+        last = nuevo;
+    }
 
 
 }
