15c2483a9be9797d4a9d8e7c24addc7acb0fc5e8
maryori190909 <maryori.armengod@estudiante.ucsm.edu.pe>
2025-05-10 22:04:29 -0500

    v7

--- Ejercicio1/StackLink.java
+++ Ejercicio1/StackLink.java
@@ -1,5 +1,16 @@
 package Ejercicio1;
 
-public class StackLink {
+public class StackLink<E> implements Stack<E> {
+    private Node<E> tope;
+
+    public StackLink() {
+        this.tope = null;
+    }
+
+    public void push(E x) {
+        Node<E> nuevo = new Node<>(x);
+        nuevo.setNext(tope);
+        tope = nuevo;
+    }
 
 }
