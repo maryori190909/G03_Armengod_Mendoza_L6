004fae74a7d50e15e97c58f89682428e1f0235cb
maryori190909 <maryori.armengod@estudiante.ucsm.edu.pe>
2025-05-10 21:38:12 -0500

    v17

--- Actividad3/Test.java
+++ Actividad3/Test.java
@@ -1,5 +1,13 @@
 package Actividad3;
 
 public class Test {
+    public static void main(String[] args) {
+        PriorityQueue<String, Integer> cl = new PriorityQueueLinkSort<>();
+
+            cl.enqueue("tarea A", 5);
+            cl.enqueue("tarea B", 1);
+            cl.enqueue("tarea C", 2);
+            cl.enqueue("tarea D", 4);
+    }
 
 }
