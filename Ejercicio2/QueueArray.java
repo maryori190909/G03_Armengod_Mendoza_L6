f4d69562203b4c288ae6b62809f6d7d5c0aefd93
maryori190909 <maryori.armengod@estudiante.ucsm.edu.pe>
2025-05-10 22:30:06 -0500

    v4

--- Ejercicio2/QueueArray.java
+++ Ejercicio2/QueueArray.java
@@ -1,5 +1,18 @@
 package Ejercicio2;
 
-public class QueueArray {
+public class QueueArray<E> implements Queue<E>{
+    private E[] array;
+    private int front;
+    private int back;
+    private int size;
+    private int capacidad;
+
+    public QueueArray(int capacidad) {
+        this.capacidad = capacidad;
+        this.array = (E[]) new Object[capacidad];
+        this.front = 0;
+        this.back = -1;
+        this.size = 0;
+    }
 
 }
