64487dd68739ff1c0b26cfb2ec39988e00ca130a
maryori190909 <maryori.armengod@estudiante.ucsm.edu.pe>
2025-05-10 20:07:34 -0500

    v8

--- Actividad2/QueueLink.java
+++ Actividad2/QueueLink.java
@@ -1,5 +1,12 @@
 package Actividad2;
+import java.util.Queue;
 
-public class QueueLink {
+import Actividad1.ExceptionIsEmpty;
+
+public class QueueLink <E> implements Queue<E>{
+    private Node<E> first;
+    private Node<E> last;
+    
+
 
 }
